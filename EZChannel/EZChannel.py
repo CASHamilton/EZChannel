a9ae76fedfba59d1f4db7946b89c8313ec8a0306
Kelton the Conqueror <crabbyhamil@gmail.com>
2020-06-30 15:54:21 -0700

    Added purge and archived files

--- EZChannel/EZChannel.py
+++ EZChannel/EZChannel.py
@@ -1,36 +1,30 @@
-
 import discord
-
-
-client = discord.Client()
-
-
+from discord.ext import commands
+x = 1
+client = commands.Bot(command_prefix = 'ez-')
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    while (x < 1000):
+        await ctx.send('test')
+        x+1
 
-    if message.content.startswith('ez-help'):
-        await message.channel.send('Hello fellow humans (or at least i hope you are . . .) \n I am made by Kelton the Conqueror. \n Commands coming soon!')
- 
-        
-#___________________________________________________________________________________________________________________________________
-
-
-
-#_________________________________________________________________________________________________________________________________
- 
+@client.command()
+async def purge(ctx, amount=500000000000000000000000000000000000000000000000):
+    await ctx.channel.purge(limit=amount)
 
 
 
 
-
-
-
+#region    
+# @client.command(aliases=['8ball'])
+# async def _8ball(ctx, *, question):
+#    responses = []
+#endregion
 
 client.run('NzI2NTMwOTcwNDU0NTg5NDcx.Xvex2A.JtXdTNKylGCZpkNeKZcqq320pBM')
